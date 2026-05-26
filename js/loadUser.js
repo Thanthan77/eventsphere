f428c4ddf3ba798e6b89f6e50b1d532a4690264a
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-26 15:50:42 -0400

    fix : boucle infini

--- js/loadUser.js
+++ js/loadUser.js
@@ -1,14 +1,20 @@
 import { supabase } from "./supabase.js";
 
 async function loadUser() {
-      const { data, error } = await supabase.auth.getUser();
+  // On récupère la session actuelle
+  const { data: { session } } = await supabase.auth.getSession();
 
-      if (error || !data.user) {
-        console.error(error);
+  //Si pas encore de session, on attend que Supabase la crée
+  if (!session) {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      if (!session) {
         window.location.href = "index.html";
-        return;
       }
-     document.getElementById("userEmail").textContent = data.user.email;
+    });
+    return;
+  }
+
+  document.getElementById("userEmail").textContent = session.user.email;
 }
 
 window.addEventListener("DOMContentLoaded", loadUser);
