1f3c18a11a2822af3ba54d927dc0c18dbfb8b2b3
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-26 14:46:21 -0400

    test login

--- js/auth.js
+++ js/auth.js
@@ -1,21 +1,22 @@
 import { supabase } from "../utils/supabaseClient.js";
 
- async function loginWithGoogle() {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-          provider: "google",
-          options: {
-            redirectTo: "http://localhost:5500/eventsphere/pages/signup.html",
-          },
-        });
+async function loginWithGoogle() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo:
+        "https://purple-tree-0bed2d91e.7.azurestaticapps.net/pages/signup.html",
+    },
+  });
 
-        if (error) {
-          console.error(error);
-          alert("Erreur Google Auth");
-        }
-      }
+  if (error) {
+    console.error(error);
+    alert("Erreur Google Auth");
+  }
+}
 
- window.addEventListener("DOMContentLoaded", () => {
-        document
-          .getElementById("google-login")
-          .addEventListener("click", loginWithGoogle);
-      });
+window.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("google-login")
+    .addEventListener("click", loginWithGoogle);
+});
