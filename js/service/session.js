53852c5da5fb4bb5ca006ea484388ad1f04575e1
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-29 10:54:08 -0400

    rectification du code pour la session de l'utilisateur

--- js/service/session.js
+++ js/service/session.js
@@ -2,17 +2,29 @@ import { supabase } from "../utils/supabase.js";
 
 
 export async function getSession() {
-  const { data } = await supabase.auth.getSession();
-  return data.session;
+  const { data: { session } } = await supabase.auth.getSession();
+
+  if (!session) return null;
+
+  // Vérifier si le user existe encore dans auth.users
+  const { data: user, error } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    await supabase.auth.signOut();
+    return null;
+  }
+
+  return session;
 }
 
 /* Rediriger si pas connecté */
 export async function requireAuth() {
   const session = await getSession();
   if (!session) {
-    window.location.href = "../index.html"; // page login
+    window.location.href = "../index.html";
   }
 }
+
 
 /* Rediriger si déjà connecté */
 export async function redirectIfLoggedIn() {
