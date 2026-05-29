2f6bd5e0fd2b33c246a2e20bd5d82705681a6a9c
Thanthan77 <ethanqc.chea@gmail.com>
2026-05-29 10:48:53 -0400

    Update login.js

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,10 +1,10 @@
+import { supabase } from "../utils/supabase.js";
 import { login, loginGoogle } from "../service/auth.js";
 import { redirectIfLoggedIn } from "../service/session.js";
 
 async function init() {
-
   await supabase.auth.signOut();
-  // utilisateur déjà connecté 
+  // utilisateur déjà connecté
   await redirectIfLoggedIn();
 
   // Connexion email
