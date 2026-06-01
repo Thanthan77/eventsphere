7b0109c11d9e63d5cbd97e8eb37b68991331f04e
Thanthan77 <ethanqc.chea@gmail.com>
2026-06-01 15:23:38 -0400

    Update events.js

--- js/service/events.js
+++ js/service/events.js
@@ -1,6 +1,6 @@
 import { supabase } from "../utils/supabase.js";
 
-export async function createEvent(title, description, isPrivate, userId,nom,courriel) {
+export async function createEvent(title, description, isPrivate, userId, nom, courriel) {
   return await supabase.from("events").insert({
     title,
     description,
@@ -14,7 +14,7 @@ export async function createEvent(title, description, isPrivate, userId,nom,cour
 export async function getEvents() {
   const { data, error } = await supabase
     .from("events")
-    .select("*")
+    .select("id, title, description, is_private, created_by, created_at")
     .order("created_at", { ascending: false });
 
   return { data, error };
@@ -23,7 +23,7 @@ export async function getEvents() {
 export async function getEventById(eventId) {
   const { data, error } = await supabase
     .from("events")
-    .select("*")
+    .select("id, title, description, is_private, created_by, nom, courriel")
     .eq("id", eventId)
     .single();
 
