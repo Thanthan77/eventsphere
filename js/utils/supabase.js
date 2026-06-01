20b0229df1a741f04a18e393ab25612f7e7abd1d
Thanthan77 <ethanqc.chea@gmail.com>
2026-06-01 13:00:01 -0400

    Update supabase.js

--- js/utils/supabase.js
+++ js/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
       export const supabase = createClient(
-        "https://nlwvesfbasdqnxsgvfkj.supabase.co",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5sd3Zlc2ZiYXNkcW54c2d2ZmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDg3ODAsImV4cCI6MjA5NTI4NDc4MH0.4HlnpeqjKkks5eslpHItSbPcjxBKcO-2FMtro7MdTcY",
+        "https://aqjqrskfaaxipixqfmqn.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxanFyc2tmYWF4aXBpeHFmbXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMjUxMzIsImV4cCI6MjA5NTkwMTEzMn0.7SQW-bI3WEjtcmN5GsYLUf5PaV-MnSOwpQmT-6RMM4s",
       );
 
