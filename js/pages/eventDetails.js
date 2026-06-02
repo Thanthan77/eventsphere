65dbfc4f406823c7e23a072e653d56b145d70699
Thanthan77 <ethanqc.chea@gmail.com>
2026-06-02 14:32:29 -0400

    Update eventDetails.js

--- js/pages/eventDetails.js
+++ js/pages/eventDetails.js
@@ -85,6 +85,7 @@ async function loadMembers(eventId) {
   const membersList = document.getElementById("members-list");
 
   const { data: members, empty, error } = await getMembers(eventId);
+  console.log("DEBUG getMembers():", members);
 
   if (error) {
     console.error("Erreur Supabase :", error);
